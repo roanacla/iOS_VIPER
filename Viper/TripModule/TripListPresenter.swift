6a52c061fb98a3139ffc5254b3dcf4cef069edc8
roanacla <roanacla@me.com>
2021-06-04 16:45:05 -0700

    Trip: Add new trip

---
+++ Viper/TripModule/TripListPresenter.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+import SwiftUI
+import Combine
+
+class TripListPresenter: ObservableObject {
+  private let interactor: TripListInteractor
+  @Published var trips: [Trip] = []
+  private var cancellables = Set<AnyCancellable>()
+  
+  
+  
+  init(interactor: TripListInteractor) {
+    self.interactor = interactor
+    interactor.model.$trips
+      .assign(to: \.trips, on: self)
+      .store(in: &cancellables)
+  }
+  
+  func makeAddNewButton() -> some View {
+    Button(action: addNewTrip) {
+      Image(systemName: "plus")
+    }
+  }
+  
+  func addNewTrip() {
+    interactor.addNewTrip()
+  }
+  
+  func deleteTrip(_ index: IndexSet) {
+    interactor.deleteTrip(index)
+  }
+}
