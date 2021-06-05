92609cd4a2c07630c2701d9d1537789a5e524440
roanacla <roanacla@me.com>
2021-06-05 16:17:10 -0700

    TripDetail: Present TripMapView with TripDetailRouter

---
+++ Viper/TripDetailModule/TripDetailRouter.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+class TripDetailRouter {
+  private let mapProvider: MapDataProvider
+  
+  init(mapProvider: MapDataProvider) {
+    self.mapProvider = mapProvider
+  }
+  
+  func makeWaypointView(for waypoint: Waypoint) -> some View {
+    let presenter = WaypointViewPresenter(waypoint: waypoint,
+                                          interactor: WaypointViewInteractor(waypoint: waypoint,
+                                                                             mapInfoProvider: mapProvider))
+    return WaypointView(presenter: presenter)
+  }
+}
--- Viper/TripDetailModule/TripDetailView.swift
+++ Viper/TripDetailModule/TripDetailView.swift
@@ -10,6 +10,18 @@ struct TripDetailView: View {
           .padding([.horizontal])
         presenter.makeMapView()
         Text(presenter.distanceLabel)
+        HStack {
+          Spacer()
+          EditButton()
+          Button(action: presenter.addWaypoint, label: {
+            Text("Add")
+          })
+        }.padding([.horizontal])
+        List {
+          ForEach(presenter.waypoints, content: presenter.cell)
+            .onMove(perform:              presenter.didMoveWaypoint(fromOffsets:toOffset:))
+            .onDelete(perform: presenter.didDeleteWaypoint(_:))
+        }
       }
       .navigationBarTitle(Text(presenter.tripName), displayMode: .inline)
       .navigationBarItems(trailing: Button("Save", action: presenter.save))
